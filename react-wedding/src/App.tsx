078b0588d390d8286ffa6677e61ed6601ff025cd
TheCodingTom <dastolto.t@gmail.com>
2025-02-18 11:20:50 +0100

    first commit

---
+++ react-wedding/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './pages/Home'
+import RSVP from './pages/RSVP'
+
+function App() {
+
+
+  return (
+    <>
+      <Home/>
+      <RSVP/>
+    </>
+  )
+}
+
+export default App
---
+++ react-wedding/src/pages/RSVP.tsx
@@ -0,0 +1,9 @@
+
+
+function RSVP() {
+  return (
+    <div>RSVP page</div>
+  )
+}
+
+export default RSVP
