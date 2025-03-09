632fa6143dbcc4a14cbdc618bf362ad79d58dfb5
TheCodingTom <dastolto.t@gmail.com>
2025-03-09 20:15:37 +0100

    added video in gallery page

--- react-wedding/src/pages/Gallery.tsx
+++ react-wedding/src/pages/Gallery.tsx
@@ -1,7 +1,23 @@
+import video from "../images/WhatsApp Video 2025-03-09 at 20.10.24.mp4"
+
 function Gallery() {
   return (
-    <div>
-      <h1>This is the gallery</h1>
+    <div className="container mt-5">
+      <h1 className="display-4 text-center mb-4">I'm sorry, there are no pictures here</h1>
+      <div className="d-flex justify-content-center mb-4">
+        <div className="border rounded shadow-sm" style={{ width: '300px', height: '300px', overflow: 'hidden' }}>
+          <iframe
+            width="100%"
+            height="100%"
+            src={video}
+            title="Embedded Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+      {/* Photo gallery grid will be added below */}
     </div>
   );
 }
