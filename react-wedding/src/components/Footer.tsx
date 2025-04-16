5499bb4a9d119321ca746b3e93ce9cd256db82ca
TheCodingTom <dastolto.t@gmail.com>
2025-04-16 08:28:52 +0200

    added footer translations

--- react-wedding/src/components/Footer.tsx
+++ react-wedding/src/components/Footer.tsx
@@ -1,6 +1,26 @@
+import { useLanguage } from "../context/LanguageContext";
+import { TranslationLang } from "../pages/Accomodations";
 import styles from "../styles/footer.module.css";
 
+const footerTranslation: Record<
+  TranslationLang,
+  { witnessesTom: string; witnessesGeri: string }
+> = {
+  en: {
+    witnessesTom: "Tom's witnesses",
+    witnessesGeri: "Geri's witnesses",
+  },
+  it: { witnessesTom: "Testimoni Tom", witnessesGeri: "Testimoni Geri" },
+  de: {
+    witnessesTom: "Tom's Trauzeugen",
+    witnessesGeri: "Geri's Trauzeuginnen",
+  },
+};
+
 function Footer() {
+  const { language } = useLanguage();
+  const { witnessesGeri, witnessesTom } =
+    footerTranslation[language as TranslationLang] || footerTranslation.en;
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
@@ -12,27 +32,27 @@ function Footer() {
         </div>
 
         <div className={styles.infoSection}>
-          <h4 className={styles.infoTitle}>Tom's witnesses</h4>
+          <h4 className={styles.infoTitle}>{witnessesTom}</h4>
           <ul className={styles.contactList}>
             <li>
-              <strong>Daniele:</strong> +49 17620202841
+              <strong>Daniele:</strong> +39 3451349563
             </li>
             <li>
-              <strong>Alen:</strong> +49 17664966479
+              <strong>Alen:</strong> +39 3476983210
             </li>
             <li>
-              <strong>Francesco:</strong> +49 17664966479
+              <strong>Francesco:</strong> +49 17663700696
             </li>
           </ul>
         </div>
         <div className={styles.infoSection}>
-          <h4 className={styles.infoTitle}>Geri's witnesses</h4>
+          <h4 className={styles.infoTitle}>{witnessesGeri}</h4>
           <ul className={styles.contactList}>
             <li>
-              <strong>Anka:</strong> +49 17620202841
+              <strong>Anka:</strong> +49 17631357072
             </li>
             <li>
-              <strong>Tabea:</strong> +49 17664966479
+              <strong>Tabea:</strong> +49 17663443148
             </li>
           </ul>
         </div>
